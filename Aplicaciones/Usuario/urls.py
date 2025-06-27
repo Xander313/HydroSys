bdd668b53d12c348e648dab216db700f46614db4
Xander313 <jhonn.roldan8626@utc.edu.ec>
2025-06-27 09:09:05 -0500

    se ha generado la ruta pra las plantillas

--- Aplicaciones/Usuario/urls.py
+++ Aplicaciones/Usuario/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    ################ ESTA RUTA ES PARA EL MENU CENTRAL##############
+    path('sesionIniciada/', views.menuCentral, name='menuCentral'), 
+
     path('perfil/', views.perfil_usuario, name='perfil_usuario'),
+
 ]
--- Aplicaciones/Usuario/views.py
+++ Aplicaciones/Usuario/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def perfil_usuario(request):
     return render(request, 'Usuario/perfil.html')
+
+
+    
+def menuCentral(request):
+    return render(request, 'Usuario/menuCentral.html')
