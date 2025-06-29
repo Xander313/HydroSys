1cd189a8da80852ca62b4b63899a608f98dd5280
Alex Iza <axel.iza2003@gmail.com>
2025-06-29 16:50:17 -0500

    agregar sensor

--- Aplicaciones/Sensor/urls.py
+++ Aplicaciones/Sensor/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import agregar_sensor
 
 urlpatterns = [
+    path('agregar_sensor/', agregar_sensor, name='agregar_sensor'),
+
 ]
--- Aplicaciones/Sensor/views.py
+++ Aplicaciones/Sensor/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Sensor
+from django.contrib import messages
 
-# Create your views here.
+def agregar_sensor(request):
+    if request.method == 'POST':
+        sensorID = request.POST.get('sensorID')
+        nombreSensor = request.POST.get('nombreSensor')
+
+        # Crear un nuevo sensor
+        nuevo_sensor = Sensor(sensorID=sensorID, nombreSensor=nombreSensor)
+        nuevo_sensor.save()
+
+        messages.success(request, 'Sensor agregado correctamente.')
+        return redirect('panel_admin')  # Cambia esto por la vista a la que deseas redirigir
+
+    return render(request, 'admin/agregar_sensor.html')  # Usa el template correcto
