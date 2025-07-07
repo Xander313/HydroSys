bad77e2f575c221a667058c298c08427ee9099a5
Alex Iza <axel.iza2003@gmail.com>
2025-07-06 20:12:54 -0500

    tablas

--- Aplicaciones/Sensor/urls.py
+++ Aplicaciones/Sensor/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import agregar_sensor
+from .views import agregar_sensor, editar_sensor, eliminar_sensor
 
 urlpatterns = [
     path('agregar_sensor/', agregar_sensor, name='agregar_sensor'),
+    path('editar_sensor/<int:id>/', editar_sensor, name='editar_sensor'),
+    path('eliminar_sensor/<int:id>/', eliminar_sensor, name='eliminar_sensor'),
+
+
 
 ]
